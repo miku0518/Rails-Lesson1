2fead3ab908e370d2229df6d7a54324768126fd6
kobae miku <kuromiku77@gmail.com>
2025-11-12 00:41:48 +0900

    スケジュール管理アプリの作成

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -10,10 +10,10 @@ class SchedulesController < ApplicationController
   def create
     @schedule = Schedule.new(params.require(:schedule).permit(:title, :start_date, :end_date, :allday, :memo))
     if @schedule.save
-      flash[:notice] = "スケジュールを登録しました"
+      flash[:notice] = "スケジュールを登録しました!"
       redirect_to  schedules_path
     else
-      flash[:alert] = "スケジュールの登録に失敗しました"
+      flash[:alert] = "スケジュールの登録に失敗しました!"
       render "new", status: :unprocessable_entity
     end
   end
@@ -29,10 +29,10 @@ class SchedulesController < ApplicationController
   def update
     @schedule = Schedule.find(params[:id])
     if @schedule.update(params.require(:schedule).permit(:title, :start_date, :end_date, :allday, :memo))
-      flash[:notice] = "予定を更新しました"
+      flash[:notice] = "予定を更新しました!"
       redirect_to schedules_path 
     else
-      flash[:alert] = "予定の更新に失敗しました" 
+      flash[:alert] = "予定の更新に失敗しました!" 
       render :edit, status: :unprocessable_entity
     end
   end
